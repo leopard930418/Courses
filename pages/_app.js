1cc527b77371e48d949cf1560f60b1329b22e6e9
leopard930418 <leopard930418@gmail.com>
2022-07-15 02:08:31 +0800

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
-import '../styles/globals.css'
+import "../styles/globals.scss";
+import React, { useEffect } from "react";
+import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CSS
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,36 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  mode: 'jit',
+  important: true,
+  darkMode: ['class', '[data-mode="dark"]'],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'title_font_family': ['ui-sans-serif', 'Poppins']
+      },
+      backgroundImage: {
+        'section1_backrgound': "url('/assets/logo.svg')",
+        'triangle': "linear-gradient(to right, #1ee9a2, #75aefc)",
+        'contact_background':"linear-gradient(149.71deg, #19F18F 15.05%, #4EC1F6 65.37%, #E582FC 95.99%)",
+        'intro_background': "linear-gradient(30.71deg, #19F18F 25.05%, #4EC1F6 70.37%, #E582FC 95.99%)",
+      },
+      
+      backgroundSize: {
+        'auto': 'auto',
+        'cover': 'cover',
+        'contain': 'contain',
+        '%50': '50%',
+        '16': '4rem'
+      },
+      z_index: {
+        '-1': '0',
+        '0': '10',
+        '1': '20',
+      },
+    },
   },
-  plugins: [],
-}
+  plugins: [require("tw-elements/dist/plugin")],
+};
