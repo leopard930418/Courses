bf822d117d0955d0c189172cace2ca352cd8c38e
leopard930418 <leopard930418@gmail.com>
2022-07-19 08:57:43 +0800

    git fupdate

--- pages/event.js
+++ pages/event.js
@@ -1,8 +1,6 @@
 import Head from "next/head";
 import Header from "../components/layout/Header";
 import Footer from "../components/layout/Footer";
-import FaqMain from "../components/Faqs/Faqmain";
-import Faq from "../components/Courses/Faq";
 import EventMain from "../components/Event/EventMain";
 
 export default function Home() {
