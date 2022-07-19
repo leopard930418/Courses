14f9afc5122346ada88b0051fb51e9a2bb064a31
leopard930418 <leopard930418@gmail.com>
2022-07-19 09:07:19 +0800

    update

--- pages/faqs.js
+++ pages/faqs.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Header from "../components/layout/Header";
 import Footer from "../components/layout/Footer";
-import FaqMain from "../components/Faqs/Faqmain";
+import FaqMain from "../components/Faqs/FaqMain";
 import Faq from "../components/Courses/Faq";
 
 export default function Home() {
