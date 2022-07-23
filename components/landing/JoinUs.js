7ea55e093ef9783036d1d600bf66f4c6fdcf6bab
leopard930418 <leopard930418@gmail.com>
2022-07-24 00:06:05 +0800

    home finish

--- components/landing/JoinUs.js
+++ components/landing/JoinUs.js
@@ -11,12 +11,32 @@ import CustomImage from "../base/CustomImage";
 export default function JoinUs() {
   return (
     <div className="bg-[#F5F5F5] h-full p-2 md:p-12">
-      <div className="bg-[#2E6D48] w-full px-2 py-10 md:p-20 rounded-md flex flex-col md:flex-row">
+      <div className="bg-[#2E6D48] w-full px-2 md:px-20 pt-10 md:pt-20 rounded-md flex flex-col md:flex-row">
         <div className="w-full md:w-1/2 flex flex-col space-y-4">
           <div className="text-2xl md:text-4xl pb-4 text-white text-left font-bold">
             Change your Learning
           </div>
-          <FormGroup>
+          <div className="flex flex-col space-y-4">
+            <div className="flex flex-row items-center">
+              <div>
+                <CustomImage src="/images/checkIcon.svg"></CustomImage>
+              </div>
+              <div className="text-white text-base md:text-xl pl-4">Loremm ipsum is simply dummy text of the planting and pye setting industry</div>
+            </div>
+            <div className="flex flex-row items-center">
+              <div>
+                <CustomImage src="/images/checkIcon.svg"></CustomImage>
+              </div>
+              <div className="text-white text-base md:text-xl pl-4">Loremm ipsum is simply dummy text of the planting and pye setting industry</div>
+            </div>
+            <div className="flex flex-row items-center">
+              <div>
+                <CustomImage src="/images/checkIcon.svg"></CustomImage>
+              </div>
+              <div className="text-white text-base md:text-xl pl-4">Loremm ipsum is simply dummy text of the planting and pye setting industry</div>
+            </div>
+          </div>
+          {/* <FormGroup>
             <FormControlLabel
               control={<Checkbox defaultChecked />}
               label="Loremm ipsum is simply dummy text of the planting and pye setting industry"
@@ -36,9 +56,9 @@ export default function JoinUs() {
               label="Loremm ipsum is simply dummy text of the planting and pye setting industry"
               className="text-white"
             />
-          </FormGroup>
+          </FormGroup> */}
 
-          <div className="py-8">
+          <div className="py-8 flex justify-center md:justify-start">
             <button className="rounded-3xl bg-black text-white py-2 px-4">
               Join Us Today
             </button>
